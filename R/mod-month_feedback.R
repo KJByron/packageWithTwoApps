fa4d0404659d04d2cb82a126745d3f65b1ecc9e1
bd-karen-byron <kare_byron@bd.com>
2024-06-10 14:12:35 -0400

    added example

--- R/mod-month_feedback.R
+++ R/mod-month_feedback.R
@@ -2,14 +2,34 @@
 #'
 #' @param id  ID
 #'
-#' @return  html for feedback string
+#' @return  html for feedback string for a given month
 #' @export
+#' 
+#' @examples
+#' \dontrun{
+#'   pos_feedback_demo <- function() {
+#'     ui <- fluidPage(month_feedback_ui("x"))
+#'     server <- function(input, output, session) {
+#'       month_feedback_server("x", month = reactive("October"))
+#'     }
+#'   shinyApp(ui, server)
+#'   }
+#'   pos_feedback_demo()
+#'   
+#'   neg_feedback_demo <- function() {
+#'     ui <- fluidPage(month_feedback_ui("x"))
+#'     server <- function(input, output, session) {
+#'       month_feedback_server("x", month = reactive("April"))
+#'     }
+#'   shinyApp(ui, server)
+#'   }
+#'   neg_feedback_demo()
+#' }
 month_feedback_ui <- function(id) {
   textOutput(NS(id, "feedback"))
 }
 
-
-#' serve for month
+#' server for month
 #'
 #' @param id  ID
 #' @param month  unit character vector with chosen month
@@ -18,7 +38,7 @@ month_feedback_ui <- function(id) {
 #' @export
 month_feedback_server <- function(id, month) {
   stopifnot(is.reactive(month))
-
+  
   moduleServer(id, function(input, output, session) {
     output$feedback <- renderText({
       if (month() == "October") {
